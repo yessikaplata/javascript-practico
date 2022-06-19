ed10b158b4be90e65c84b284168d899a57f76398
Yessika Plata <yessikaliliana@gmail.com>
2022-06-19 15:57:54 -0500

    Taller #3: Promedio, mediana y moda

--- README.md
+++ README.md
@@ -15,3 +15,10 @@
 - Segundo paso: Implementar fórmulas en Javascript
 - Tercer paso: Crear funciones
 - Cuarto paso: Integrar Javascript con HTML
+
+## Taller #2: Promedio, mediana y moda
+
+- Primer paso: definir las fórmulas
+- Segundo paso: Implementar fórmulas en Javascript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar Javascript con HTML
---
+++ js/promedios.js
@@ -0,0 +1,71 @@
+function calculateArithmeticAverage(list) {
+    const sum = list.reduce((partialSum, value) => partialSum + Number(value), 0);
+    const average = sum / list.length;
+    return Math.round(average*100)/100;
+}
+
+function calculateMedian(list) {
+    //Sort the list
+    list.sort((a, b) => a - b);
+    //Calculate middle index
+    const indexMedian = Math.trunc(list.length / 2);
+
+    //if size of list is odd return middle number
+    if (list.length % 2 != 0) {
+        return list[indexMedian];
+    }
+
+    //if size of list is even return arithmetic average for middle numbers
+    const elementMedian1 = list[indexMedian - 1];
+    const elementMedian2 = list[indexMedian];
+    const medians = [elementMedian1, elementMedian2];
+    return calculateArithmeticAverage(medians);
+}
+
+function calculateMode(list) {
+    console.log(list);
+    listCount = {};
+    //Generates object with counter by each number in list
+    list.map((value) => {
+        if (!listCount[value]) {
+            listCount[value] = 0;
+        }
+        listCount[value]++;
+    })
+    console.log(listCount);
+    // Order list by counter in object
+    const listOrderedByCount = Object.entries(listCount).sort(
+        function (a, b) {
+            return a[1] - b[1];
+        }
+    )
+    //Return the last number in ordered list
+    return listOrderedByCount.pop()[0];
+}
+
+function onClickButtonCalculateAverage() {
+    const dataStr = document.getElementById("inputDataAverage").value;
+    const lblAverage = document.getElementById("lblAverage");
+    const dataList = dataStr.split(",").map((item)=>item.trim());
+    const average = calculateArithmeticAverage(dataList)
+    lblAverage.textContent = `La media es: ${average} `;
+}
+
+
+function onClickButtonCalculateMedian() {
+    const dataStr = document.getElementById("inputDataMedian").value;
+    const lblMedian1 = document.getElementById("lblMedian1");
+    const lblMedian2 = document.getElementById("lblMedian2");
+    const dataList = dataStr.split(",").map((item)=>item.trim());
+    const median = calculateMedian(dataList);
+    lblMedian1.textContent = `La lista ordenada es: ${dataList.sort((a, b) => a - b)} `;
+    lblMedian2.textContent = `La mediana es: ${median} `;
+}
+
+function onClickButtonCalculateMode() {
+    const dataStr = document.getElementById("inputDataMode").value;
+    const lblMode = document.getElementById("lblMode");
+    const dataList = dataStr.split(",").map((item)=>item.trim());
+    const mode = calculateMode(dataList);
+    lblMode.textContent = `La moda es: ${mode} `;
+}
